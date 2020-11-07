1004096e7b8720f10c83840598b473135b9847f4
wagyourtail <wagyourtail@wagyourtail.xyz>
2020-11-07 11:59:44 -0700

    update

--- src/main/java/xyz/wagyourtail/jsmacroslua/functions/FConsumerLua.java
+++ src/main/java/xyz/wagyourtail/jsmacroslua/functions/FConsumerLua.java
@@ -1,13 +1,14 @@
 package xyz.wagyourtail.jsmacroslua.functions;
 
-import java.util.List;
-
 import org.luaj.vm2.LuaClosure;
+import org.luaj.vm2.LuaValue;
 import org.luaj.vm2.lib.jse.CoerceJavaToLua;
-
+import org.luaj.vm2.lib.jse.CoerceLuaToJava;
 import xyz.wagyourtail.jsmacros.extensionbase.Functions;
 import xyz.wagyourtail.jsmacros.extensionbase.IFConsumer;
 import xyz.wagyourtail.jsmacros.extensionbase.MethodWrapper;
+
+import java.util.List;
 
 public class FConsumerLua extends Functions implements IFConsumer<LuaClosure, LuaClosure, LuaClosure> {
     
@@ -21,9 +22,48 @@ public class FConsumerLua extends Functions implements IFConsumer<LuaClosure, Lu
     }
     
     @Override
-    public MethodWrapper<Object, Object> autoWrap(LuaClosure c) {
-        return new MethodWrapper<Object, Object>() {
-
+    public MethodWrapper<Object, Object, Object> autoWrap(LuaClosure c) {
+        return new MethodWrapper<Object, Object, Object>() {
+    
+            @Override
+            public Object get() {
+                return CoerceLuaToJava.coerce(c.call(), Object.class);
+            }
+    
+            @Override
+            public boolean test(Object o) {
+                return c.call(CoerceJavaToLua.coerce(o)).toboolean();
+            }
+    
+            @Override
+            public Object apply(Object o) {
+                return CoerceLuaToJava.coerce(c.call(CoerceJavaToLua.coerce(o)), Object.class);
+            }
+    
+            @Override
+            public boolean test(Object o, Object o2) {
+                return c.call(CoerceJavaToLua.coerce(o), CoerceJavaToLua.coerce(o2)).toboolean();
+            }
+    
+            @Override
+            public Object apply(Object o, Object o2) {
+                return CoerceLuaToJava.coerce(c.call(CoerceJavaToLua.coerce(o), CoerceJavaToLua.coerce(o2)), Object.class);
+            }
+    
+            @Override
+            public int compare(Object o1, Object o2) {
+                LuaValue val = c.call(CoerceJavaToLua.coerce(o1), CoerceJavaToLua.coerce(o2));
+                if (val.isnumber()) {
+                    return val.toint();
+                }
+                throw new RuntimeException("could not convert lua value to integer");
+            }
+    
+            @Override
+            public void run() {
+                c.call();
+            }
+    
             @Override
             public void accept(Object arg0) {
                 c.call(CoerceJavaToLua.coerce(arg0));
@@ -38,9 +78,51 @@ public class FConsumerLua extends Functions implements IFConsumer<LuaClosure, Lu
     }
     
     @Override
-    public MethodWrapper<Object, Object> autoWrapAsync(LuaClosure c) {
-        return new MethodWrapper<Object, Object>() {
-            
+    public MethodWrapper<Object, Object, Object> autoWrapAsync(LuaClosure c) {
+        return new MethodWrapper<Object, Object, Object>() {
+    
+            @Override
+            public Object get() {
+                return CoerceLuaToJava.coerce(c.call(), Object.class);
+            }
+    
+            @Override
+            public boolean test(Object o) {
+                return c.call(CoerceJavaToLua.coerce(o)).toboolean();
+            }
+    
+            @Override
+            public Object apply(Object o) {
+                return CoerceLuaToJava.coerce(c.call(CoerceJavaToLua.coerce(o)), Object.class);
+            }
+    
+            @Override
+            public boolean test(Object o, Object o2) {
+                return c.call(CoerceJavaToLua.coerce(o), CoerceJavaToLua.coerce(o2)).toboolean();
+            }
+    
+            @Override
+            public Object apply(Object o, Object o2) {
+                return CoerceLuaToJava.coerce(c.call(CoerceJavaToLua.coerce(o), CoerceJavaToLua.coerce(o2)), Object.class);
+            }
+    
+            @Override
+            public int compare(Object o1, Object o2) {
+                LuaValue val = c.call(CoerceJavaToLua.coerce(o1), CoerceJavaToLua.coerce(o2));
+                if (val.isnumber()) {
+                    return val.toint();
+                }
+                throw new RuntimeException("could not convert lua value to integer");
+            }
+    
+            @Override
+            public void run() {
+                Thread t = new Thread(() -> {
+                    c.call();
+                });
+                t.start();
+            }
+    
             @Override
             public void accept(Object arg0) {
                 Thread t = new Thread(() -> {
@@ -61,22 +143,22 @@ public class FConsumerLua extends Functions implements IFConsumer<LuaClosure, Lu
     }
 
     @Override
-    public MethodWrapper<Object, Object> toAsyncBiConsumer(LuaClosure arg0) {
+    public MethodWrapper<Object, Object, Object> toAsyncBiConsumer(LuaClosure arg0) {
         return autoWrapAsync(arg0);
     }
 
     @Override
-    public MethodWrapper<Object, Object> toAsyncConsumer(LuaClosure arg0) {
+    public MethodWrapper<Object, Object, Object> toAsyncConsumer(LuaClosure arg0) {
         return autoWrapAsync(arg0);
     }
 
     @Override
-    public MethodWrapper<Object, Object> toBiConsumer(LuaClosure arg0) {
+    public MethodWrapper<Object, Object, Object> toBiConsumer(LuaClosure arg0) {
         return autoWrap(arg0);
     }
 
     @Override
-    public MethodWrapper<Object, Object> toConsumer(LuaClosure arg0) {
+    public MethodWrapper<Object, Object, Object> toConsumer(LuaClosure arg0) {
         return autoWrap(arg0);
     }
 }
