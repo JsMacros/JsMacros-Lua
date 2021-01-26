a0c7b2d1b541f4a7190688ecdf0907d1780ec9ec
wagyourtail <wagyourtail@wagyourtail.xyz>
2021-01-26 11:39:49 -0700

    fix another source bug

--- src/main/java/xyz/wagyourtail/jsmacros/lua/luaj/mixins/MixinLuaClosure.java
+++ src/main/java/xyz/wagyourtail/jsmacros/lua/luaj/mixins/MixinLuaClosure.java
@@ -4,6 +4,7 @@ import org.luaj.vm2.LuaClosure;
 import org.luaj.vm2.LuaError;
 import org.luaj.vm2.Prototype;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -16,7 +17,21 @@ public class MixinLuaClosure {
     
     @Inject(method = "processErrorHooks", at = @At("TAIL"), remap = false)
     private void onProcessErrorHooks(LuaError le, Prototype p, int pc, CallbackInfo ci) {
-        ((ILuaError) le).setFile(p.source != null ? new File(p.source.tojstring()) : null);
+        if (p.source != null) {
+            String cid = chunkId(p.source.tojstring());
+            if (cid != null) {
+                ((ILuaError) le).setFile(p.source != null ? new File(cid) : null);
+            }
+        }
         ((ILuaError) le).setLine(p.lineinfo != null && pc > 0 && pc < p.lineinfo.length ? p.lineinfo[pc] : -1);
+    }
+    
+    @Unique
+    private String chunkId(String source) {
+        if (source.startsWith("=") || source.startsWith("@")) {
+            return source.substring(1);
+        } else {
+            return null;
+        }
     }
 }
