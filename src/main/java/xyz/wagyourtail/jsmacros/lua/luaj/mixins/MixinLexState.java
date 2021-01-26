ccf92b91c55cbbf6c86ae964cea141f776650f58
wagyourtail <wagyourtail@wagyourtail.xyz>
2021-01-26 10:30:45 -0700

    fix trimming of file name

--- src/main/java/xyz/wagyourtail/jsmacros/lua/luaj/mixins/MixinLexState.java
+++ src/main/java/xyz/wagyourtail/jsmacros/lua/luaj/mixins/MixinLexState.java
@@ -1,11 +1,11 @@
 package xyz.wagyourtail.jsmacros.lua.luaj.mixins;
 
-import org.luaj.vm2.Lua;
 import org.luaj.vm2.LuaError;
 import org.luaj.vm2.LuaString;
 import org.luaj.vm2.compiler.LexState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -22,10 +22,22 @@ public class MixinLexState {
     
     @Inject(method = "lexerror", at = @At(value = "INVOKE", target = "Lorg/luaj/vm2/LuaError;<init>(Ljava/lang/String;)V", remap = false), remap = false)
     void lexError(String msg, int token, CallbackInfo ci) {
+        System.out.println(source.tojstring());
         LuaError e = new LuaError(msg);
         ((ILuaError) e).setLine(linenumber);
-        ((ILuaError) e).setFile(new File(Lua.chunkid(source.tojstring())));
+        String cid = chunkId(source.tojstring());
+        if (cid != null) {
+            ((ILuaError) e).setFile(new File(cid));
+        }
         throw e;
+    }
+    
+    @Unique private String chunkId(String source) {
+        if (source.startsWith("=") || source.startsWith("@")) {
+            return source.substring(1);
+        } else {
+            return null;
+        }
     }
     
 }
