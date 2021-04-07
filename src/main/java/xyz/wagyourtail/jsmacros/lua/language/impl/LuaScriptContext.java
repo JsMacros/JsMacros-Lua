7ee07d9bed39af415c84655312d06cfabc2caf12
wagyourtail <wagyourtail@wagyourtail.xyz>
2021-04-06 18:21:59 -0700

    actually kill the threads

--- src/main/java/xyz/wagyourtail/jsmacros/lua/language/impl/LuaScriptContext.java
+++ src/main/java/xyz/wagyourtail/jsmacros/lua/language/impl/LuaScriptContext.java
@@ -1,6 +1,7 @@
 package xyz.wagyourtail.jsmacros.lua.language.impl;
 
 import org.luaj.vm2.Globals;
+import xyz.wagyourtail.jsmacros.core.Core;
 import xyz.wagyourtail.jsmacros.core.language.ScriptContext;
 
 public class LuaScriptContext extends ScriptContext<Globals> {
@@ -14,6 +15,7 @@ public class LuaScriptContext extends ScriptContext<Globals> {
     @Override
     public synchronized void closeContext() {
         closed = true;
+        Core.instance.threadContext.entrySet().stream().filter(e -> e.getValue() == this).forEach(e -> e.getKey().interrupt());
     }
     
 }
